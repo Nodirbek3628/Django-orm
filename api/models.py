334c7d16af50da348ebd27a1580d9b1a9e168fef
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-11-23 17:30:55 +0500

    create products api

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField( max_length=150)
     category = models.CharField( max_length=150)
-    price  = models.IntegerField()
+    price  = models.DecimalField(max_digits=5, decimal_places=2)
     quantity  = models.IntegerField()
     rating = models.DecimalField(max_digits=5, decimal_places=2)
     active = models.BooleanField(default=True)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,43 @@
-from django.http import HttpRequest,JsonResponse
+import json
 
+from django.http import HttpRequest,JsonResponse
+from .models import Product
+
+def to_json(product:Product)->dict:
+    return {
+        "id":product.id,
+        "name":product.name,
+        "category":product.category,
+        "price": product.price,
+        "quantity":product.quantity,
+        "rating":product.rating,
+        "active":product.active,
+        "create_at":product.create_at.strftime('%d/%m/%Y, %H:%M:%S'),
+        "update_at":product.update_at.strftime('%d/%m/%Y, %H:%M:%S')   #DateTimeni stringga o'tgarib beradi
+
+}
 
 
 def product_view(request:HttpRequest)->JsonResponse:
+
+    if request.method == "POST":
+        result = []
+
+        data = json.loads(request.body.decode())
+        for item in data:
+
+        
+            new_product = Product(
+                name = item.get('name', ''),
+                category = item.get('category', ''),
+                price  = item.get('price', 0),
+                quantity = item.get('quantity', 0),
+                rating = item.get('rating', 0)
+            )
+            new_product.save()
+            result.append(to_json(new_product))
+
+        return JsonResponse(data={'result':result, 'count':len(result)},status=201)
+
     
-    return JsonResponse(data={'ok':'ok'})
+    return JsonResponse(data={})
